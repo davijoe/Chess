6780cfebbecf86df3ca750fa32fb2ddde598a0e0
Selmerr <115707577+Selmerr@users.noreply.github.com>
2024-04-18 00:05:17 +0200

    made some move logic for all pieces except pawn. Haven't implemented en passant or castling yet, but will do it soon

--- src/robot/Board.java
+++ src/robot/Board.java
@@ -2,7 +2,15 @@ package robot;
 
 public class Board {
 
-    char[][] board = new char[8][8];
+    int[][] board = new int[8][8];
+
+    int generateMoveCounter = 0;
+
+    int enPassant = 1;
+
+    int longCastle = 2;
+
+    int shortCastle = 3;
 
     Move moves = new Move(100);
     char currentPlayer;
@@ -11,33 +19,185 @@ public class Board {
         return board[row][col] == 0;
     }
 
-    public void generateMoves() {
+    public void generateMoves(Board board) {
 
+        if (currentPlayer == 'w') {
+            for (int row = 0; row<8; row++) {
+                for (int col = 0; col < 8; col++) {
+                    if (board.board[row][col] == 1) {
+                        generateRookMoves(row, col);
+                    }
 
-        
+                    if (board.board[row][col] == 2) {
+                        generateKnightMoves(row,col);
+                    }
+
+                    if (board.board[row][col] == 3) {
+                        generateBishopMoves(row, col);
+                    }
+
+                    if (board.board[row][col] == 4) {
+                        generateQueenMoves(row, col);
+                    }
+
+                    if (board.board[row][col] == 5) {
+                        generateKingMoves(row, col);
+                    }
+
+                    if (board.board[row][col] == 6) {
+                        generatePawnMoves(row, col);
+                    }
+                }
+            }
+
+        }
+
     }
 
 
 
-    public void generateRookMoves() {
+    public void generateRookMoves(int row, int col) {
+        int[][] directions = {{-1,0}, {1,0}, {0,1}, {0,-1}};
+        int counter = 0;
+        for (int[] direction : directions) {
+            int rowNew = row + direction[0];
+            int colNew = col + direction[1];
+            while (0<=rowNew && rowNew<8 && 0<=colNew && colNew<8) {
+                //Checking if im dumb
+                System.out.println(counter);
+                counter++;
+                if(isTileEmpty(row,col)) {
+                    moves.moves[generateMoveCounter][0] = row;
+                    moves.moves[generateMoveCounter][1] = col;
+                    moves.moves[generateMoveCounter][2] = rowNew;
+                    moves.moves[generateMoveCounter][3] = colNew;
+                    moves.moves[generateMoveCounter][4] = 1;
+                    generateMoveCounter++;
+                }
+                rowNew += direction[0];
+                colNew += direction[1];
+            }
+        }
 
     }
 
-    public void generateBishopMoves() {
+    public void generateBishopMoves(int row, int col) {
+        int[][] directions = {{-1,1}, {1,1}, {1,-1}, {-1,-1}};
+        int counter = 0;
+        for (int[] direction : directions) {
+            int rowNew = row + direction[0];
+            int colNew = col + direction[1];
+            while (0<=rowNew && rowNew<8 && 0<=colNew && colNew<8) {
+                //Checking if im dumb
+                System.out.println(counter);
+                counter++;
+                if(isTileEmpty(row,col)) {
+                    moves.moves[generateMoveCounter][0] = row;
+                    moves.moves[generateMoveCounter][1] = col;
+                    moves.moves[generateMoveCounter][2] = rowNew;
+                    moves.moves[generateMoveCounter][3] = colNew;
+                    moves.moves[generateMoveCounter][4] = 3;
+                    generateMoveCounter++;
+                }
+                rowNew += direction[0];
+                colNew += direction[1];
+            }
+        }
+
 
     }
-    public void generateKnightMoves() {
+    public void generateKnightMoves(int row, int col) {
+        int[][] directions = {{2,1}, {2,-1}, {1,2}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {1,-2}};
+        int counter = 0;
+        for (int[] direction : directions) {
+            int rowNew = row + direction[0];
+            int colNew = col + direction[1];
+            if (0<=rowNew && rowNew<8 && 0<=colNew && colNew<8) {
+                //Checking if im dumb
+                System.out.println(counter);
+                counter++;
+                if(isTileEmpty(row,col)) {
+                    moves.moves[generateMoveCounter][0] = row;
+                    moves.moves[generateMoveCounter][1] = col;
+                    moves.moves[generateMoveCounter][2] = rowNew;
+                    moves.moves[generateMoveCounter][3] = colNew;
+                    moves.moves[generateMoveCounter][4] = 2;
+                    generateMoveCounter++;
+                }
+            }
+        }
 
     }
-    public void generateQueenMoves() {
+    public void generateQueenMoves(int row, int col) {
+        int[][] directions = {{-1,1}, {1,1}, {1,-1}, {-1,-1}, {-1,0}, {1,0}, {0,1}, {0,-1}};
+        int counter = 0;
+        for (int[] direction : directions) {
+            int rowNew = row + direction[0];
+            int colNew = col + direction[1];
+            while (0<=rowNew && rowNew<8 && 0<=colNew && colNew<8) {
+                //Checking if im dumb
+                System.out.println(counter);
+                counter++;
+                if(isTileEmpty(row,col)) {
+                    moves.moves[generateMoveCounter][0] = row;
+                    moves.moves[generateMoveCounter][1] = col;
+                    moves.moves[generateMoveCounter][2] = rowNew;
+                    moves.moves[generateMoveCounter][3] = colNew;
+                    moves.moves[generateMoveCounter][4] = 4;
+                    generateMoveCounter++;
+                }
+                rowNew += direction[0];
+                colNew += direction[1];
+            }
+        }
 
     }
-    public void generateKingMoves() {
-
+    public void generateKingMoves(int row, int col) {
+        int[][] directions = {{-1,1}, {1,1}, {1,-1}, {-1,-1}, {-1,0}, {1,0}, {0,1}, {0,-1}};
+        int counter = 0;
+        for (int[] direction : directions) {
+            int rowNew = row + direction[0];
+            int colNew = col + direction[1];
+            if (0<=rowNew && rowNew<8 && 0<=colNew && colNew<8) {
+                //Checking if im dumb
+                System.out.println(counter);
+                counter++;
+                if(isTileEmpty(row,col)) {
+                    moves.moves[generateMoveCounter][0] = row;
+                    moves.moves[generateMoveCounter][1] = col;
+                    moves.moves[generateMoveCounter][2] = rowNew;
+                    moves.moves[generateMoveCounter][3] = colNew;
+                    moves.moves[generateMoveCounter][4] = 5;
+                    generateMoveCounter++;
+                }
+            }
+        }
     }
 
-    public void generatePawnMoves() {
+    public void generatePawnMoves(int row, int col) {
+
+
+//        int[][] directions = new int[2][2];
+//        if (board[row][col] == 6) {
+//            directions[0][0] = 1;
+//        }
+//        int counter = 0;
+//        for (int[] direction : directions) {
+//            int rowNew = row + direction[0];
+//            int colNew = col + direction[1];
+//            while (0<=rowNew && rowNew<8 && 0<=colNew && colNew<8) {
+//                //Checking if im dumb
+//                System.out.println(counter);
+//                counter++;
+//                if(isTileEmpty(row,col)) {
+//                    moves.moves[generateMoveCounter][0] = row;
+//                    moves.moves[generateMoveCounter][1] = col;
+//                    moves.moves[generateMoveCounter][2] = rowNew;
+//                    moves.moves[generateMoveCounter][3] = colNew;
+//                    moves.moves[generateMoveCounter][4] = 2;
+//                    generateMoveCounter++;
+//                }
+//            }
+        }
 
     }
-
-}
